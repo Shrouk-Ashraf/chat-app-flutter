60a17c1662b4fbc583e323edf56f890edd855b0c
Shrouk <shrouk.a.elsaeid31@gmail.com>
2023-12-20 01:54:51 +0200

    provide login cubit to login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:chat_app/cubits/login_cubit/login_cubit.dart';
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/login_page.dart';
 import 'package:chat_app/pages/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,6 +14,7 @@ void main() async {
   );
   runApp(const ChatApp());
 }
+
 class ChatApp extends StatelessWidget {
   const ChatApp({Key? key}) : super(key: key);
 
@@ -20,9 +23,14 @@ class ChatApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        LoginPage.id:(context)=>LoginPage(),
-        RegisterPage.id :(context)=>RegisterPage(),
-        ChatPage.id : (context)=> ChatPage(),
+        LoginPage.id: (context) {
+          return BlocProvider(
+            create: (context) => LoginCubit(),
+            child: const LoginPage(),
+          );
+        },
+        RegisterPage.id: (context) => RegisterPage(),
+        ChatPage.id: (context) => ChatPage(),
       },
       initialRoute: LoginPage.id,
     );
