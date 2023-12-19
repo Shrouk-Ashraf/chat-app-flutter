84fd194858ff5d2bdd2db81e3ff98629363cba50
Shrouk <shrouk.a.elsaeid31@gmail.com>
2023-12-20 01:36:16 +0200

    add login cubit

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,19 +1,20 @@
-import 'dart:convert';
-
 import 'package:chat_app/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class MessageModel{
+class MessageModel {
   final String message;
   final Timestamp createdAt;
   final String senderEmail;
 
-  MessageModel({required this.message,required this.createdAt, required this.senderEmail});
+  MessageModel(
+      {required this.message,
+      required this.createdAt,
+      required this.senderEmail});
 
-  factory MessageModel.fromJson(jsonData){
+  factory MessageModel.fromJson(jsonData) {
     return MessageModel(
-        message: jsonData[kMessageKey],
-        createdAt: jsonData[kCreatedAtKey],
+      message: jsonData[kMessageKey],
+      createdAt: jsonData[kCreatedAtKey],
       senderEmail: jsonData[kSenderEmailKey],
     );
   }
