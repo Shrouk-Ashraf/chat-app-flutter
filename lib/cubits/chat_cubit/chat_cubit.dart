981300cf0d81e7b53d418ca49b0c85a91e485f06
Shrouk <shrouk.a.elsaeid31@gmail.com>
2023-12-22 20:05:35 +0200

    add chat functions to the chat cubit

--- lib/cubits/chat_cubit/chat_cubit.dart
+++ lib/cubits/chat_cubit/chat_cubit.dart
@@ -1,8 +1,32 @@
-import 'package:bloc/bloc.dart';
+import 'package:chat_app/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
   ChatCubit() : super(ChatInitial());
+
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(kMessagesCollection);
+
+  sendMessage({required String message, required String email}) {
+    if (message.isNotEmpty) {
+      messages.add({
+        kMessageKey: message,
+        kCreatedAtKey: DateTime.now(),
+        kSenderEmailKey: email,
+      });
+    }
+  }
+
+  getMessages() {
+    messages
+        .orderBy(kCreatedAtKey, descending: true)
+        .snapshots()
+        .listen((event) {
+      emit(ChatSendingSuccess());
+    });
+  }
 }
